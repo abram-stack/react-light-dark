2c19e942288590774137a74be8ef1b412ac5cdf0
abram-stack <anthonybramantyo@outlook.com>
2023-04-11 20:52:41 +0200

    finishing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Main from '../components/Main';
 import Navbar from '../components/Navbar';
+import { useState } from 'react';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  function toggleDarkMode() {
+    setDarkMode((prevMode) => !prevMode);
+  }
   return (
     <div className='container'>
-      <Navbar />
-      <Main />
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
